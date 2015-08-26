02bd2b49e6cf57a13600ff509a7e7d760ba9160c
cmeurer <carriemeurer@gmail.com>
2015-08-26 12:57:32 -0600

    space below footer

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 			<?php echo esc_attr(get_theme_mod('footer_text', 'Gravit Theme powered by WordPress')); ?>
 			Silage Breeding is part of the <a href="http://agronomy.wisc.edu/" target="_blank">Agronomy Department</a> of the <a href="http://www.wisc.edu/" target="_blank">University of Wisconsin-Madison</a>.<br />
 			Copyright &copy;2006 The Board of Regents of the University of Wisconsin System.<br />
-			Feedback, questions or accessibility issues: <a href="mailto:ndeleongatti@wisc.edu">ndeleongatti@wisc.edu</a>.
+			Feedback, questions or accessibility issues: <a href="mailto:ndeleongatti@wisc.edu">ndeleongatti@wisc.edu</a>.<br /><br />
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
