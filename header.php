ab6f131f094b5c71474b283f5c4b53f8f8432792
cmeurer <carriemeurer@gmail.com>
2015-08-28 13:13:45 -0600

    front page instead of home

--- header.php
+++ header.php
@@ -48,8 +48,7 @@
 			 <?php if(function_exists(simple_breadcrumb) && !is_front_page()) {simple_breadcrumb();} ?>
 			 
 			 <!-- full width slider -->	
-					<?php wp_reset_query(); ?>
-					<?php if ( is_home() ):?>
+					<?php if ( is_front_page() ):?>
     					echo do_shortcode("[metaslider id=360]"); 
 					<?php endif; ?>
 					
